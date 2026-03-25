2c670788a4f4757dc4cc31856b62677dd10f2370
PaulinaVaz <vazquez.encina.paulina@gmail.com>
2026-03-25 11:52:24 -0600

    Middleware de roles registrado y rutas actualizadas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ClinicaController;
 use App\Http\Controllers\EspecialistaController;
+use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 // Ruta principal que carga el index
@@ -10,4 +12,13 @@ Route::get('/', [ClinicaController::class, 'index']);
 // Ruta de contacto con nombre para que el botón funcione
 Route::get('/contacto', [ClinicaController::class, 'contact'])->name('contacto');
 
-Route::resource('especialistas', EspecialistaController::class);
+// Solo el Admin entra aquí
+Route::group(['middleware' => ['role:ADMIN']], function () {
+    Route::resource('usuarios', UsuarioController::class);
+    Route::resource('especialistas', EspecialistaController::class);
+});
+
+// Admin y Especialista pueden ver esto
+Route::group(['middleware' => ['role:ADMIN,ESPECIALISTA']], function () {
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+});
